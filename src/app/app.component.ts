497a1e67ad57a92eff10637404f7cdd046f64bc2
Fernando Ponce <data.fernan@gmail.com>
2024-05-07 20:21:18 -0500

    mi primera practica angular dev tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SecondaryComponent } from './secondary/secondary.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SecondaryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -12,6 +13,8 @@ export class AppComponent {
   title = 'app-practica';
   image = 'assets/images.jpg'
   icecreams = ['Mora', 'Fresa']
-  
+
+  stringboton="1"
+
 
 }
--- src/app/secondary/secondary.component.ts
+++ src/app/secondary/secondary.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-secondary',
@@ -9,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class SecondaryComponent {
 
+  @Input() botonname = '';
+
 }
+
+
