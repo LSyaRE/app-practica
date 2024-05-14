26c980f1d814b16625ef4d89c6255021e1854731
LSyaRE <jonasvillacis@yahoo.com>
2024-05-13 21:12:17 -0500

    add routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { SecondaryComponent } from './secondary/secondary.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SecondaryComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/secondary/secondary.component.ts
+++ src/app/secondary/secondary.component.ts
@@ -9,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class SecondaryComponent {
 
+  option: string = '' 
+
+
+  changeOption(){
+    this.option = {2: '1',1: '2'}[this.option] ?? '1';
+
+  }
+
+
 }
